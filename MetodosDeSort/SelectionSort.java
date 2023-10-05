d388ca70ea3ad3d188af9d98f94d207459a78b9b
Vitor Cosso <vitorcosso@gmail.com>
2023-10-05 15:39:48 -0300

    implementando o metodo Selection sort

--- MetodosDeSort/SelectionSort.java
+++ MetodosDeSort/SelectionSort.java
@@ -1,5 +1,7 @@
 package MetodosDeSort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
     /*
      * Selection sort é um bubble sort mais inteligente.
@@ -8,9 +10,32 @@ public class SelectionSort {
      * Na comparacao ele guarda o menor e depois faz a troca com o i.
      * entao garante assim que o i seja o menor. e realiza i +1 para o avanco.
      * 
+     * 
      */
 
      public static void main(String[] args) {
-        
+        int[] arr  = new int[10];
+        for(int i =0; i < arr.length ; i++){
+            arr[i] = (int) (Math.random() * 10);
+        }
+        System.out.println(Arrays.toString(arr));
+
+        int aux, menor_posicao;
+        for(int i =0; i< arr.length ; i++){
+            menor_posicao=i;
+            for(int j = i+1; j< arr.length ;j++){
+                // se o arr[posicao menor] for MAIOR que o arr[j], significa que tem que trocar a posicao
+                if(arr[j] < arr[menor_posicao] ){
+                    menor_posicao = j;
+                }
+
+                aux = arr[i];
+                arr[i] = arr[menor_posicao];
+                arr[menor_posicao] = aux;
+
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
      }
 }
